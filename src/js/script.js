f6da75d2b5b50f43755f69d91d94f55f2b829692
Vitalii <dvornikvitaliy@gmail.com>
2020-01-24 15:28:40 +0200

    8 section full

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1000,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/rigth.svg"></button>'
+    });
+});
