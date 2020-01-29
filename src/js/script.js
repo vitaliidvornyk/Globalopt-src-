061952f1f797b9edc01a37dc9f9d93f075ff1f4b
Vitalii <dvornikvitaliy@gmail.com>
2020-01-29 19:08:30 +0200

    992px

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1000,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/rigth.svg"></button>'
-    });
+	$('.carousel__inner').slick({
+		speed: 1000,
+		adaptiveHeight: true,
+		prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="../icons/rigth.svg"></button>'
+	});
 });
 
-$(window).on( 'load', function() {
-    $('html, body').animate({scrollTop : 0});
-});
+// $(window).on( 'load', function() {
+//     $('html, body').animate({scrollTop : 0});
+// });
