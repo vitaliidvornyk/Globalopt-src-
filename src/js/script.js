232c63d6939e32cd67b82901053761ebf4b43130
Vitalii <dvornikvitaliy@gmail.com>
2020-01-31 00:28:50 +0200

    768px

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,6 @@
+
+// Slider
+
 $(document).ready(function(){
 	$('.carousel__inner').slick({
 		speed: 1000,
@@ -6,6 +9,28 @@ $(document).ready(function(){
 		nextArrow: '<button type="button" class="slick-next"><img src="../icons/rigth.svg"></button>'
 	});
 });
+
+// Menu and hamburger
+
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.delivery__header-menu'),
+    menuItem = document.querySelectorAll('.delivery__header-item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('delivery__header-menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('delivery__header-menu_active');
+        });
+    });
+});
+
+// Scroll up
 
 // $(window).on( 'load', function() {
 //     $('html, body').animate({scrollTop : 0});
